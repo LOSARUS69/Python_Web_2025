89e38c112b50a2dde409953f3593840827875a40
Manvel <a@b.ru>
2025-06-26 14:28:31 +0300

    Match Case

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-# match - case (3.10 >)
-height = int(input('Введите рост: '))
+# match - case (работает с версии 3.10 >)
+flag = True
+print('Возможные ходы:\n\tL - влево\n\tR - вправо\n\tF - прямо,\n\tQ - выход')
 
-while not (150 <= height <= 180):
-    print(f'Рост кандидата {height} не подходит')
-    height = int(input('Введите рост: '))
-
-print('Кандидат выбран')
+while flag:
+    ch = input('Ваш выбор: ')
+    match ch:
+        case 'L' | 'l' | 'д' | 'Д':
+            print('Свернули налево')
+        case 'R' | 'к' | 'r' | 'К':
+            print('Свернули направо')
+        case 'F' | 'а' | 'А' | 'f':
+            print('Пошли прямо')
+        case 'Q' | 'q' | 'й' | 'Й':
+            print('До свидания!')
+            flag = False
+        case _:  # default
+            print('Выбор не ясен')
