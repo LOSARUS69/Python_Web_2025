5ddb06118f2759d05b95c0c69258ce7951fc1aff
Manvel <a@b.ru>
2025-06-26 12:06:00 +0300

    Loop Continue (morj)

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 # Циклы:
-# while
-# while <условие>:
-#     команды
-# for
-counter = 0  # обнуляем счётчик
-# цикл из 5 итераций
-while counter < 5:
-    print(f'Итерация номер: {counter + 1}')
-    # counter = counter + 1  # инкремент
-    counter += 1 # инкремент (краткая запись)
-print(f'Итого в counter уже {counter}')
-print('Обратный отсчёт:')
-while counter > 0:
-    print(f'Итерация номер: {counter}')
-    counter -= 1  # декремент (краткая запись)
+while len(word := input('Введите слово не короче 3 символов: ')) <= 3:
+    print(f'Слово: "{word}" слишком короткое.')
+print(f'Вы ввели слово: "{word}"')
