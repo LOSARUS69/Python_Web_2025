ca751a8991058857b8ff41f8554dd927f1dfbbaa
Manvel <a@b.ru>
2025-06-26 15:49:46 +0300

    Nested Loops

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# factorial
-N = 12
-fact = 1
+# Вложенные циклы
 
-for i in range(1, N + 1):
-    fact *= i
-
-print(fact)
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(f'{i} * {j} = {i * j}', end='\t')
+    print()
