2633250bf9e0db265756137377a01c42d405a2a0
Manvel <a@b.ru>
2025-06-26 14:37:54 +0300

    Keyword "in"

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# вывод чисел, закаивающихся на 3 в диапазоне от 1 до 100
-counter = 1
+# ключевое слово in
+word = 'поток'
 
-while counter <= 100:
-    if counter % 10 == 3:
-        print(counter, end=', ')
-    counter += 1
+if 'ток' in word:
+    print('Есть')
