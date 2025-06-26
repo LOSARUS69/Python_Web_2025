396731a7f53fb413c0d1f57045d06a954bea2548
Manvel <a@b.ru>
2025-06-26 16:16:18 +0300

    Selection

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-# Вложенные циклы
+# Подбор по росту
+# 150 < height < 180
+# Число кандидатов
+# Число, кто прошел по критерию
+# Среди прошедших min и max
 
-for i in range(1, 10):
-    for j in range(1, 10):
-        print(f'{i} * {j} = {i * j}', end='\t')
-    print()
+total = 0
+total_success = 0
+total_unsuccess = 0
+min_val = float('inf')
+max_val = float('-inf')
+
+while (num := int(input('Введите рост: '))) != -1:
+    if 150 <= num <= 180:
+        total_success += 1
+        if min_val > num:
+            min_val = num
+        if num > max_val:
+            max_val = num
+    total += 1
+
+print(f'Число кандидатов: {total}')
+print(f'Число прошедших отбор: {total_success}')
+print(f'Минимальный рост: {min_val}')
+print(f'Максимальный рост: {max_val}')
