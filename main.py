9cb8493d5597ddd35c1119b5d3fedca353bb3b38
Manvel <a@b.ru>
2025-06-26 14:35:36 +0300

    Select Numbers

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-# match - case (работает с версии 3.10 >)
-flag = True
-print('Возможные ходы:\n\tL - влево\n\tR - вправо\n\tF - прямо,\n\tQ - выход')
+# вывод чисел, закаивающихся на 3 в диапазоне от 1 до 100
+counter = 1
 
-while flag:
-    ch = input('Ваш выбор: ')
-    match ch:
-        case 'L' | 'l' | 'д' | 'Д':
-            print('Свернули налево')
-        case 'R' | 'к' | 'r' | 'К':
-            print('Свернули направо')
-        case 'F' | 'а' | 'А' | 'f':
-            print('Пошли прямо')
-        case 'Q' | 'q' | 'й' | 'Й':
-            print('До свидания!')
-            flag = False
-        case _:  # default
-            print('Выбор не ясен')
+while counter <= 100:
+    if counter % 10 == 3:
+        print(counter, end=', ')
+    counter += 1
