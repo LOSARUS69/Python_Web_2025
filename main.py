3745c2d0c139ab76f846beec24cf0dd4b16f3969
Manvel <a@b.ru>
2025-06-27 12:14:35 +0300

    Set Cards

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-# Города
-s = set()
+# Сдаём карты
+cards = {3, 7, 'туз', 'валет', 'король', 'дама'}
 
-while (city := input('Назовите город: ')) != '':
-    if city in s:
-        print('Такой город уже был')
-    else:
-        s.add(city)
-
-print(f'Итого было названо: {len(s)} городов:')
-for item in s:
-    print('\t', item)
+while cards: # пока карты в колоде есть
+    print(cards.pop())
