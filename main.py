3fe9e8550beb4f52b5f2be19a5c5aa4a98202928
Manvel <a@b.ru>
2025-06-27 13:55:10 +0300

    Set remove all cards besides ace

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-# PEP8 - правила именования
-# c, l, O, I
-# Операции над множествами
-a = {3, 5, 7}
-b = {3, 5, 7, 9, 11}
+# Удаление всех кард, кроме туза
+cards = {3, 7, 'T', 'D', 'V', 'K'}
+# ace = {'T'}
+#
+# result = cards - ace
+# print(result)
 
-# Объединение множеств
-c = a.union(b)
-# c = a | b
-print(c)
+t_is = False
+# 2 вариант
+while cards:
+    card = cards.pop()
+    if card == 'T':
+        cards.add(card)
+        t_is = True
+    else:
+        print(card)
 
-# Пересечение
-c = a.intersection(b)  # и там, и там
-# c = a & b
-print(c)
-
-# Разность
-c = b.difference(a)  # есть в 1-м, но нет во 2-м
-# c = b - a
-print(c)
-
-# Симметричная разность
-c = a.symmetric_difference(b)  # есть только в одном из двух
-# c = b ^ a
-print(c)
+    if t_is and len(cards) == 1:
+        break
