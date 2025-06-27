c15de279fefd726d05b37c975f3ea15819d84cbd
Manvel <a@b.ru>
2025-06-27 12:07:16 +0300

    Set Cities

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
-# Коллекции (set, list, dict, tuple)
-# Множества
-s = set()  # пустое множество
-# print(dir(s)) - список методов множества
-s = {'3', '5', '7', '3', '3', 3}
-s.add(3) # добавление
-# Удаление
-s.remove('3') # вызывает ошибку если нет
-s.discard('3') # удаляет вслепую
-# s.clear() # очищает множество
-temp = s.pop() # удаляет случайный и возвращает его
-print(temp)
-print(type(s))  # класс
-print(f'Число элементов в s = {len(s)}')
-print('Присутствует ли 3')
-if str(3) in s:
-    print('Да')
-else:
-    print('Нет')
+# Города
+s = set()
+
+while (city := input('Назовите город: ')) != '':
+    if city in s:
+        print('Такой город уже был')
+    else:
+        s.add(city)
+
+print(f'Итого было названо: {len(s)} городов:')
 for item in s:
-    if item == '3':
-        print(item)
-# 'add', 'clear', 'copy', 'difference',
-# 'difference_update', 'discard',
-# 'intersection', 'intersection_update',
-# 'isdisjoint', 'issubset', 'issuperset',
-# 'pop', 'remove', 'symmetric_difference',
-# 'symmetric_difference_update', 'union',
-# 'update']
+    print('\t', item)
