f0867ba08e217f4fd69332ef35e775143932e43d
Manvel <a@b.ru>
2025-06-27 15:42:42 +0300

    Encrypting 1

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 # Строки (immutable, iterable)
-# Таблица символов
-s = '\xB0'  # значок градуса
-u = '\u2603'  # снеговик
-
 # две удобные функции
 # ord(символ) - возвращает код символа в Unicode
 # chr(код) - возвращает символа по Unicode-коду
 
-print(u)
-print('25' + s + 'C')
-print(f'Код снеговика в Unicode: {ord('☃')}')
+s = set()
+word = input('Введите фразу для зашифровки: ')
+
+# Зашифровываем
+for ch in word:
+    s.add(ord(ch))
+
+print(s)
+
+# Расшифровываем
+res = ''
+for i in s:
+    res += chr(i)
+
+print(res)
