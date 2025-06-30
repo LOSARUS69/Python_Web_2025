f5f2d32a21008e29556e59cd3515030774c33436
Manvel <a@b.ru>
2025-06-30 14:06:12 +0300

    List Intro

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-# Строки (immutable, iterable)
-# Cрез (у строки и у других коллекций, кроме set)
-# [начало:окончание:шаг]
+# Cписки (list)
 
-s = 'Дорог Рим'  # + и *
-# Город Миргород
+# lst = []  # пустой список
+lst = [1, 7, 3, 5, 6, 4]
+lst.append(2)
+lst.sort()
+lst.reverse()
 
-temp = s.lower()
-city = temp[:5][::-1]
-res =  city + ' ' + temp[6:][::-1] + city
+print(lst)
 
-print(res.title())
+a = ['a', 'b', 'c']
+b = a[:]  # a.copy()
+b.append('d')  # b += ['d']
+print(id(a))
+print(id(b))
+print(a)
+print(b)
 
-
-
+['append', 'clear', 'copy', 'count', 'extend',
+ 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
