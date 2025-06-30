6f351cef9466ed17f4efa25d227490fc4c5fdae7
Manvel <a@b.ru>
2025-06-30 16:36:37 +0300

    Remove All Spaces

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 # Кортеж (tuple, immutable)
 # Методы строки split() и join()
-from os import lstat
-
-text = '192 и также 168 и также 0 и также 1'
-ip = '192.168.0.1'
-
-lst = text.split(' и также ')
-
-print(lst)
-text2 = ' и также '.join(lst)
-
-print(text2)
-
-
+text = '   P   y   t h  o  n   '
+res = ''.join(text.split())  # убрать все пробелы
+print(res)
