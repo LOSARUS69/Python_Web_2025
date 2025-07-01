49f00df38c04928957795eabb678dad80cba5f25
Manvel <a@b.ru>
2025-07-01 10:24:29 +0300

    Stop Words 3

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # ДЗ
-# Фраза: ну я типо вообще короче не понимаю этот язык
+# Фраза: ну?, я типо, вообще: короче, не понимаю этот язык!
+commas = (',', '!', '.', '?', '-', ':')
 stop_words = {'ну', 'типо', 'короче', 'не'}
 message = input('Введите сообщение: ')
-lst = message.split() # все слова
+for z in commas:
+    message = message.replace(z, '')
+lst = message.split()  # все слова
 res = sorted(set(lst) - stop_words)
 for a, b in enumerate(res, 1):
     print(f'{a}. {b}')
