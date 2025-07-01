4f96e2404f650223ec4e844e3c05821ad54c9aa4
Manvel <a@b.ru>
2025-07-01 12:19:31 +0300

    Nested lists generation 2

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # Вложенные списки
 matrix = []
-table = []
 
 start = 1
 N = 4
 
 for i in range(N):
+    table = []
     for j in range(start, start + N):
         table.append(j)
     matrix.append(table)
-    table = []
     start += N
 
 print(matrix)
