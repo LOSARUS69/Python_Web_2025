eb269e383006952aafb7f542b686c7f3db658869
Manvel <a@b.ru>
2025-07-01 10:09:27 +0300

    Stop Words 1

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-# Кортеж (tuple, immutable)
-# Методы строки split() и join()
-text = '   P   y   t h  o  n   '
-res = ''.join(text.split())  # убрать все пробелы
-print(res)
+# ДЗ
+# Фраза: ну я типо вообще короче не понимаю этот язык
+stop_words = ['ну', 'типо', 'короче']
+temp = []
+message = input('Введите сообщение: ')
+lst = message.split() # все слова
+for item in lst:
+    if item not in stop_words:
+        temp.append(item)
+res = sorted(temp)
+for a, b in enumerate(res, 1):
+    print(f'{a}. {b}')
