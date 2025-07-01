8210dbf12bc58c0e90fcd3696030057c841a5f71
Manvel <a@b.ru>
2025-07-01 11:28:32 +0300

    Сomprehension task

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 # Списочные выражения (list comprehension)
+# Занести в список каждое третье слово из предложения
+text = 'Списочные выражения применяются для эффективности кода'
+
+# res = [a for a in text.split() if (text.index(a) + 1) % 3 == 0]
+res = [a for a in text.split()[2::3]]
+# операции со списком
+print(res)
+
+
+
+
+
+
+
+
 # squares = []
 # for i in range(10):
 #     squares.append(i ** 2)
@@ -7,17 +22,19 @@
 # squares = [i ** 2 for i in range(10)]
 
 # список квадратов чётных чисел
-squares = [i ** 2 for i in range(10) if i % 2 == 0]
-print(*squares, sep=', ')
+# squares = [i ** 2 for i in range(10) if i % 2 == 0]
+# print(*squares, sep=', ')
+#
+# # произведение i и j
+# print([i * j for i in range(3) for j in range(3)])
+#
+# n = '100 200 300 400 500 600 700 800 900'
+# approved = [500, 800]
+# a = [int(i) for i in n.split() if int(i) in approved]
+# # какие-то действия со списком a
+# print(a)
 
-# произведение i и j
-print([i * j for i in range(3) for j in range(3)])
-
-n = '100 200 300 400 500 600 700 800 900'
-approved = [500, 800]
-a = [int(i) for i in n.split() if int(i) in approved]
-# какие-то действия со списком a
-print(a)
+############################################################
 
 # # Фраза: ну?, я типо, вообще: короче, не понимаю этот язык!
 # commas = (',', '!', '.', '?', '-', ':')
