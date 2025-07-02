7ffbbfc852b49b316eb9005c993ad4538273a4d9
Manvel <a@b.ru>
2025-07-02 15:43:45 +0300

    Args and Kwargs

--- main.py
+++ main.py
@@ -41,10 +41,31 @@ def sandwich(type_of_meal, with_onion=False, with_tomato=False):
     print('Булочка')
 
 
-sandwich('котлета', with_onion=True)
-print(fio(surname='Бендер', name='Остап'))
-print(multy(2, 3, 4))
-print(calc(1, 2, 3, operator='*'))
+def print_any(*args, **kwargs):
+    for i in args:
+        print(i)
+    for k, v in kwargs.items():
+        print(k, '=', v)
+
+
+def profile(name, surname, city, *children, **additional):
+    print('Имя:', name)
+    print(f'Фамилия: {surname}')
+    print(f'Из города: {city}')
+    if len(children) > 0:
+        print('Дети:', ', '.join(children))
+    if 'hobbie' in additional:
+        print('Хобби:', ', '.join(additional['hobbie']))
+    # print(additional)
+
+
+profile('Дмитрий', 'Колесов', 'Волгоград',
+        'Мария', 'Пётр', hobbie=['Филателия', 'Шахматы'])
+# print_any('Дмитрий', 'Колесов', city='Москва', age=27)
+# sandwich('котлета', with_onion=True)
+# print(fio(surname='Бендер', name='Остап'))
+# print(multy(2, 3, 4))
+# print(calc(1, 2, 3, operator='*'))
 
 # При распаковке '*' может быть только одна
 # def coordinates() -> tuple:
