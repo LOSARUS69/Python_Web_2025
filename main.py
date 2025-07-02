fb90527ae4fe4958743a14c88fa6b6d2e83dabda
Manvel <a@b.ru>
2025-07-02 11:23:21 +0300

    Local, Global and main()

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 # Области видимости
+PI = 3.1415
+# Shadows name 'square' from outer scope
+def greet(name):
+    print('Привет,', name)
+    name = 'друг'
+    print('Здравствуй,', name)
+
+
+def square_area(length: int, width: int) -> None:
+    area = length * width
+    print(f'Площадь площади = {area}')
+
+
+def circle_length(radius: float):
+    perimetr = 2 * PI * radius
+    print(f'Длина окружности с радиусом {radius} = {perimetr:.2f}')
+
+
+def print_array(array: list) -> None:
+    for item in array:
+        print(item)
+
+
+# Главная функция
+def main():
+    area = 'Дворцовая площадь'
+    words = ['Привет', 'мир']
+    greet('Пётр')
+    print('Давай встретимся, где', area)
+    square_area(320, 240)
+    print('Ну что? Встречаемся, где', area)
+    circle_length(5)
+    print_array(words)
+    print_array(['a', 'b', 'c'])
+
+
+main()
+
 # Пример как делать не надо
-a = [1, 2]
-
-def change_array():
-    a[0] = 0
-
-change_array()
-print(a)
+# a = [1, 2]
+#
+# def change_array():
+#     a[0] = 0
+#
+# change_array()
+# print(a)
 
 # # Функция с аннотацией
 # num_to_str = {
