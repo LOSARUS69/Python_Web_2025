dd86ce1c32701f228634038735b9d9507e85d691
Manvel <a@b.ru>
2025-07-04 12:46:19 +0300

    Модуль datetime

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 # Встроенные библиотеки
 # PyPI - Python Package Index (pypi.org)
 
+import datetime as dt
+
+my_time = dt.time(15, 27, 32)
+print(my_time)
+my_day = dt.date(2025, 7, 4)
+print(my_day)
+my_day_time = dt.datetime.combine(my_day, my_time)
+print(my_day_time)
+
+date1 = dt.date(2025, 6, 15)
+date2 = dt.date(2025, 7, 3)
+delta = date2 - date1
+
+print(delta)
+
+# print(dt.datetime.now())
+# print(dt.datetime.now().date())
+# print(dt.datetime.now().time())
+
+# time = dt.datetime.now()
+# ftime = time.strftime('%d-%m-%Y')
+# print('Сегодня:', ftime)
+# ftime = time.strftime('%H:%M:%S')
+# print('Время:', ftime)
+
 # import random as r
 # r.seed()
 # print(r.random())
