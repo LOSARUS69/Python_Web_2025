8af6ff92a28bb83624465703c3bb9f273ac60797
Manvel <a@b.ru>
2025-07-04 11:12:20 +0300

    Модуль math

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 # Встроенные библиотеки
+# PyPI - Python Package Index (pypi.org)
+# import math as m
+# from math import *
+from math import pi, sqrt, sin, radians
+# from math import sqrt
 
-lst = [1, 1, 2, 3, 5]
-# res = 0
-# for x in lst:
-#     res += x
-res = sum(lst)
-min_value = min(lst)
-max_value = max(lst)
-print(res, min_value, max_value)
+# print(dir(m))
+# print(help(m.cos))
+
+print('Число Пи:', pi)
+print('Квадратный корень 4:', sqrt(4))
+print('Синус 30°:', round(sin(radians(30)), 2))
+
+# lst = [1, 1, 2, 3, 5]
+# # res = 0
+# # for x in lst:
+# #     res += x
+# res = sum(lst)
+# min_value = min(lst)
+# max_value = max(lst)
+# print(res, min_value, max_value)
 # import sys
 #
 # strings = [d.strip('\n') for d in sys.stdin.readlines()]
