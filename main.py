302a90d6087dab37d0e8586a1238696fb232d23a
Manvel <a@b.ru>
2025-07-07 11:31:17 +0300

    Docx Template

--- main.py
+++ main.py
@@ -1,45 +1,61 @@
 # Внешние библиотеки
-# Документы
-# Word - DOCX (python-docx)
+# Документы по шаблону (template.docx)
+# Word - DOCX (docxtpl)
 # pip freeze > requirements.txt - создание файла зависимости
 # pip install -r requirements.txt - установка списка библиотек
-from docx import Document
-from docx.enum.text import WD_ALIGN_PARAGRAPH
-from docx.shared import Cm, Inches, Mm, Pt # Для размеров
+from docxtpl import DocxTemplate
 
-doc = Document()  # создание экземпляра документа
+# Загрузка шаблона
+doc = DocxTemplate('docs/template.docx')
 
-# Добавление заголовка
-doc.add_heading('Отчёт за месяц', 1)
-paragraph = doc.add_paragraph()
-paragraph = doc.add_paragraph('В этом отчёте представлены')
-# run - что-то внутри абзаца
-paragraph.add_run(' ключевые показатели').bold = True
-# Новый абзац для списка
-paragraph = doc.add_paragraph()
-paragraph_format = paragraph.paragraph_format
-paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
-# Маркированный
-paragraph = doc.add_paragraph('Первый пункт', style='List Bullet')
-paragraph = doc.add_paragraph('Второй пункт', style='List Bullet')
+# Данные для подстановки в шаблон
+content = {
+    'company': 'OOO "Монолит"',
+    'employee': 'Петров Д.И.',
+    'position': 'Менеджер',
+    'date': '01/01/2025'
+}
 
-# Нумерованный
-paragraph = doc.add_paragraph('Первый пункт', style='List Number')
-paragraph = doc.add_paragraph('Второй пункт', style='List Number')
+doc.render(content)
+doc.save('docs/about.docx')
 
-paragraph = doc.add_paragraph()
-# Добавляем таблицу
-table = doc.add_table(rows=3, cols=3)
-
-# Заполняем
-for i, row in enumerate(table.rows):
-    for j, cell in enumerate(table.columns):
-        cell.text = f'Строка {i + 1}, Столбец {j + 1}'
-
-doc.add_paragraph()
-doc.add_picture('images/sunny_day.jpg', width=Mm(105))
-
-doc.save('docs/report.docx')
+# from docx import Document
+# from docx.enum.text import WD_ALIGN_PARAGRAPH
+# from docx.shared import Cm, Inches, Mm, Pt # Для размеров
+#
+# doc = Document()  # создание экземпляра документа
+#
+# # Добавление заголовка
+# doc.add_heading('Отчёт за месяц', 1)
+# paragraph = doc.add_paragraph()
+# paragraph = doc.add_paragraph('В этом отчёте представлены')
+# # run - что-то внутри абзаца
+# paragraph.add_run(' ключевые показатели').bold = True
+# # Новый абзац для списка
+# paragraph = doc.add_paragraph()
+# paragraph_format = paragraph.paragraph_format
+# paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
+# # Маркированный
+# paragraph = doc.add_paragraph('Первый пункт', style='List Bullet')
+# paragraph = doc.add_paragraph('Второй пункт', style='List Bullet')
+#
+# # Нумерованный
+# paragraph = doc.add_paragraph('Первый пункт', style='List Number')
+# paragraph = doc.add_paragraph('Второй пункт', style='List Number')
+#
+# paragraph = doc.add_paragraph()
+# # Добавляем таблицу
+# table = doc.add_table(rows=3, cols=3)
+#
+# # Заполняем
+# for i, row in enumerate(table.rows):
+#     for j, cell in enumerate(table.columns):
+#         cell.text = f'Строка {i + 1}, Столбец {j + 1}'
+#
+# doc.add_paragraph()
+# doc.add_picture('images/sunny_day.jpg', width=Mm(105))
+#
+# doc.save('docs/report.docx')
 
 # from PIL import Image, ImageFilter, ImageEnhance
 #
