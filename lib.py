a1c4fcacb93bc16d678f3df9f833f35a98331cb3
Manvel <a@b.ru>
2025-07-09 15:05:59 +0300

    OOP Static Members

--- lib.py
+++ lib.py
@@ -1,9 +1,12 @@
 class Car:
+    counter = 0  # статичное свойство (счетчик машин)
+
     def __init__(self, brand='Noname', model='NoModel', color='black'):
         self.brand = brand  # 'Skoda'
         self.model = model  # 'Octavia'
         self.color = color  # 'red'
         self.engine_on = False
+        Car.counter += 1
 
     def start_engine(self):
         self.engine_on = True
@@ -13,6 +16,38 @@ class Car:
             print(f'Едем в {place} на {self.brand} {self.model}')
         else:
             print('Двигатель не заведён, не едем')
+
+    @staticmethod
+    def get_counter():
+        return Car.counter
+
+
+class Person:
+    def __init__(self, name='Bill', age=1):
+        # свойства (поля) класса
+        self._name = name
+        self._age = age
+
+    # setters
+    def set_name(self, new_name):
+        if new_name:
+            self._name = new_name
+
+    def set_age(self, new_age):
+        if 0 < new_age < 150:
+            self._age = new_age
+        else:
+            print('Некорректный возраст — ', new_age)
+
+    # getters
+    def get_name(self):
+        return self._name
+
+    def get_age(self):
+        return self._age
+
+    def person_info(self):
+        print(f'Человек с именем {self._name}. Возраст: {self._age}')
 
 
 def summ(a, b):
--- main.py
+++ main.py
@@ -1,37 +1,19 @@
 # OOП (encapsulation)
-# Геттеры и сеттеры
-class Person:
-    def __init__(self, name='Bill', age=1):
-        # свойства (поля) класса
-        self._name = name
-        self._age = age
+# Статичные члены класса
+from lib import Car
 
-    # setters
-    def set_name(self, new_name):
-        if new_name:
-            self._name = new_name
+car1 = Car()
+car2 = Car()
+car3 = Car()
 
-    def set_age(self, new_age):
-        if 0 < new_age < 150:
-            self._age = new_age
-        else:
-            print('Некорректный возраст — ', new_age)
+print('В парке машин:', Car.get_counter())
 
-    # getters
-    def get_name(self):
-        return self._name
-
-    def get_age(self):
-        return self._age
-
-    def person_info(self):
-        print(f'Человек с именем {self._name}. Возраст: {self._age}')
-
-
-p = Person()
-p.set_age(7897)
-print(p.get_name())
-p.person_info()
+# from lib import Person
+#
+# p = Person()
+# p.set_age(7897)
+# print(p.get_name())
+# p.person_info()
 
 # from lib import Car
 #
